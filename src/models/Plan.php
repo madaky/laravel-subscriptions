ed600b8ff5c485f955408a204043d614743da5c2
kbtechlabs <kbtechlabs@outlook.com>
2019-09-25 23:42:22 +0530

    changed filable to gaurded

--- src/models/Plan.php
+++ src/models/Plan.php
@@ -7,10 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Plan extends Model {
     use \kbtechlabs\LaravelSubscriptions\Traits\UseUuid;
 
-    protected $fillable = [
-        'id','name','price','durations','limit','limit_use','start_at','ends_at'
-    ];
-
+    protected $guarded=['id'];
     public function __construct(array $attributes = []) {
         parent::__construct($attributes);
         $this->table = config('laravel-subscriptions.plan');
